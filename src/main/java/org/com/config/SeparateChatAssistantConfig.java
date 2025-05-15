6c259daeb9e5dfc7d6c2a627c47ad4cda0fab700
17639638825 <562985990@qq.com>
2025-05-15 20:05:14 +0800

    提交

---
+++ src/main/java/org/com/config/SeparateChatAssistantConfig.java
@@ -0,0 +1,17 @@
+package org.com.config;
+
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeparateChatAssistantConfig {
+@Bean
+ChatMemoryProvider chatMemoryProvider() {
+    return memoryId -> MessageWindowChatMemory.builder()
+            .id(memoryId)
+            .maxMessages(10)
+            .build();
+}
+}
