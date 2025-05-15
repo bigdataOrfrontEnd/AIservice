3b5a2d78abd2ee361ae1f1ef9f1aec4a5e092e41
17639638825 <562985990@qq.com>
2025-05-15 20:13:27 +0800

    提交

--- src/main/java/org/com/assistant/Assistant.java
+++ src/main/java/org/com/assistant/Assistant.java
@@ -1,5 +1,12 @@
 package org.com.assistant;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+
 //接口里面实现方法
 public interface Assistant {
     String chat(String userMessage);
-}
+};
--- src/test/java/org/com/LlamTest.java
+++ src/test/java/org/com/LlamTest.java
@@ -1,6 +1,7 @@
 package org.com;
 
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import org.com.assistant.SeparateChatAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,5 +19,17 @@ public class LlamTest {
         String answer = ollamaChatModel.chat("你是谁");
         //输出结果
         System.out.println(answer);
+    }
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+
+    @Test
+    public void testChatMemory5() {
+        String answer1 = separateChatAssistant.chat(1,"我是环环");
+        System.out.println(answer1);
+        String answer2 = separateChatAssistant.chat(1,"我是谁");
+        System.out.println(answer2);
+        String answer3 = separateChatAssistant.chat(2,"我是谁");
+        System.out.println(answer3);
     }
 }
