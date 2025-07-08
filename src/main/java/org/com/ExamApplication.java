ccf7351baf21d95997ea308da198329b94ef6361
前端 <562985990@qq.com>
2025-07-08 22:32:01 +0800

    merge

---
+++ src/main/java/org/com/ExamApplication.java
@@ -0,0 +1,18 @@
+package org.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class
+})
+public class ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+        System.out.println("Hello World");
+    }
+}
--- src/main/java/org/com/assistant/xiaozhiAgent.java
+++ src/main/java/org/com/assistant/xiaozhiAgent.java
@@ -1,16 +1,16 @@
-package org.com.assistant;
-
-import dev.langchain4j.service.MemoryId;
-import dev.langchain4j.service.UserMessage;
-import dev.langchain4j.service.spring.AiService;
-
-import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
-
-@AiService(
-        wiringMode = EXPLICIT,
-        chatModel = "OllamaChatModel",
-        chatMemoryProvider = ""
-)
-public interface xiaozhiAgent {
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
-}
+//package org.com.assistant;
+//
+//import dev.langchain4j.service.MemoryId;
+//import dev.langchain4j.service.UserMessage;
+//import dev.langchain4j.service.spring.AiService;
+//
+//import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
+//
+//@AiService(
+//        wiringMode = EXPLICIT,
+//        chatModel = "OllamaChatModel",
+//        chatMemoryProvider = "chatMemoryProvider"
+//)
+//public interface xiaozhiAgent {
+//    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+//}
